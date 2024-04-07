88f5ce7902b5532af7994446f9a30e4cc4037d06
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2024-04-07 19:48:25 +0700

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,5 @@
+fruits = ["heo" , "bo" , "ga"]
+x , y , z = fruits
+print("x: ", x,)
+print("y: ", y)
+print("z: ", z)
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+a=5
+b=6
+print(a)
+a = "chau"
+print(a)

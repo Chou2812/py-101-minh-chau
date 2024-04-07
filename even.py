e25213c224cb7cf130c49a87043407832e59a869
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2024-04-07 20:28:46 +0700

    bai tap

---
+++ even.py
@@ -0,0 +1,31 @@
+x = 5
+if x%2==0:
+    print("day la so chan")
+else:
+    print("dday la so le")
+    
+    
+    
+y = [1 ,2 ,3 ,4, 15, 67, 89, 23]
+for i in y:
+    if i % 2 == 0:
+        print("{} la so chan".format(i))
+    else:
+        print("{} la so le".format(i))
+        
+        
+def sum(a,b):
+    c = a + b
+    print(c)
+sum(3,5)
+
+ac = 500
+for i in range(ac):
+    print(i + 1)
+    
+yc = 1
+while yc <= 500:
+    print(yc)
+    yc = yc + 1
+    
+    
